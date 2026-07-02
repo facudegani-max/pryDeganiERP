b3a4702b956aa0d848f722d41cbf4d051c1fd296
facudegani-max <facudegani@gmail.com>
2026-07-02 09:34:55 -0300

    Fin

--- pryDeganiERP/Auditoria.Designer.cs
+++ pryDeganiERP/Auditoria.Designer.cs
@@ -34,7 +34,20 @@
             this.chkbuttonAscendente = new System.Windows.Forms.RadioButton();
             this.chkbuttonDescendente = new System.Windows.Forms.RadioButton();
             this.cmblistaAuditoria = new System.Windows.Forms.ComboBox();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.cmbHasta = new System.Windows.Forms.ComboBox();
+            this.cmbDesde = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAuditoria)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvAuditoria
@@ -61,7 +74,7 @@
             // chkbuttonAscendente
             // 
             this.chkbuttonAscendente.AutoSize = true;
-            this.chkbuttonAscendente.Location = new System.Drawing.Point(189, 21);
+            this.chkbuttonAscendente.Location = new System.Drawing.Point(6, 23);
             this.chkbuttonAscendente.Name = "chkbuttonAscendente";
             this.chkbuttonAscendente.Size = new System.Drawing.Size(107, 20);
             this.chkbuttonAscendente.TabIndex = 13;
@@ -72,7 +85,7 @@
             // chkbuttonDescendente
             // 
             this.chkbuttonDescendente.AutoSize = true;
-            this.chkbuttonDescendente.Location = new System.Drawing.Point(307, 21);
+            this.chkbuttonDescendente.Location = new System.Drawing.Point(119, 23);
             this.chkbuttonDescendente.Name = "chkbuttonDescendente";
             this.chkbuttonDescendente.Size = new System.Drawing.Size(117, 20);
             this.chkbuttonDescendente.TabIndex = 14;
@@ -83,10 +96,85 @@
             // cmblistaAuditoria
             // 
             this.cmblistaAuditoria.FormattingEnabled = true;
-            this.cmblistaAuditoria.Location = new System.Drawing.Point(18, 20);
+            this.cmblistaAuditoria.Location = new System.Drawing.Point(6, 19);
             this.cmblistaAuditoria.Name = "cmblistaAuditoria";
             this.cmblistaAuditoria.Size = new System.Drawing.Size(140, 24);
             this.cmblistaAuditoria.TabIndex = 15;
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Location = new System.Drawing.Point(6, 21);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(140, 24);
+            this.cmbEstado.TabIndex = 18;
+            // 
+            // cmbHasta
+            // 
+            this.cmbHasta.FormattingEnabled = true;
+            this.cmbHasta.Location = new System.Drawing.Point(6, 21);
+            this.cmbHasta.Name = "cmbHasta";
+            this.cmbHasta.Size = new System.Drawing.Size(140, 24);
+            this.cmbHasta.TabIndex = 17;
+            // 
+            // cmbDesde
+            // 
+            this.cmbDesde.FormattingEnabled = true;
+            this.cmbDesde.Location = new System.Drawing.Point(6, 21);
+            this.cmbDesde.Name = "cmbDesde";
+            this.cmbDesde.Size = new System.Drawing.Size(140, 24);
+            this.cmbDesde.TabIndex = 16;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbDesde);
+            this.groupBox1.Location = new System.Drawing.Point(174, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(155, 55);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Desde";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbHasta);
+            this.groupBox2.Location = new System.Drawing.Point(335, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(155, 55);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hasta";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cmbEstado);
+            this.groupBox3.Location = new System.Drawing.Point(744, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(155, 55);
+            this.groupBox3.TabIndex = 21;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Estado";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.cmblistaAuditoria);
+            this.groupBox4.Location = new System.Drawing.Point(13, 0);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(155, 55);
+            this.groupBox4.TabIndex = 22;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Tablas";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.chkbuttonAscendente);
+            this.groupBox5.Controls.Add(this.chkbuttonDescendente);
+            this.groupBox5.Location = new System.Drawing.Point(497, 0);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(241, 55);
+            this.groupBox5.TabIndex = 23;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Orden";
             // 
             // Auditoria
             // 
@@ -94,9 +182,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSlateBlue;
             this.ClientSize = new System.Drawing.Size(977, 618);
-            this.Controls.Add(this.cmblistaAuditoria);
-            this.Controls.Add(this.chkbuttonDescendente);
-            this.Controls.Add(this.chkbuttonAscendente);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.dgvAuditoria);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -106,8 +196,13 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Consultar Base Datos";
             ((System.ComponentModel.ISupportInitialize)(this.dgvAuditoria)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -118,5 +213,13 @@
         private System.Windows.Forms.RadioButton chkbuttonAscendente;
         private System.Windows.Forms.RadioButton chkbuttonDescendente;
         private System.Windows.Forms.ComboBox cmblistaAuditoria;
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.ComboBox cmbHasta;
+        private System.Windows.Forms.ComboBox cmbDesde;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
     }
 }
--- pryDeganiERP/Auditoria.cs
+++ pryDeganiERP/Auditoria.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.OleDb;
+using System.Globalization;
 using System.Windows.Forms;
 
 namespace pryDeganiERP
@@ -21,11 +22,17 @@ namespace pryDeganiERP
             this.btnSalir.Click += btnSalir_Click;
 
             this.cmblistaAuditoria.SelectedIndexChanged += CmblistaAuditoria_SelectedIndexChanged;
+            this.cmbDesde.SelectedIndexChanged += (s, e) => CargarAuditoria();
+            this.cmbHasta.SelectedIndexChanged += (s, e) => CargarAuditoria();
+            this.cmbEstado.SelectedIndexChanged += (s, e) => CargarAuditoria();
             this.chkbuttonAscendente.CheckedChanged += SortingOptionChanged;
             this.chkbuttonDescendente.CheckedChanged += SortingOptionChanged;
 
             // Make controls non-editable by user
             cmblistaAuditoria.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbDesde.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbHasta.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbEstado.DropDownStyle = ComboBoxStyle.DropDownList;
             dgvAuditoria.ReadOnly = true;
             dgvAuditoria.AllowUserToAddRows = false;
             dgvAuditoria.AllowUserToDeleteRows = false;
@@ -47,7 +54,65 @@ namespace pryDeganiERP
         private void Auditoria_Load(object sender, EventArgs e)
         {
             CargarListaTablas();
+            CargarFiltros();
             CargarAuditoria();
+        }
+
+        private void CargarFiltros()
+        {
+            try
+            {
+                bd.AbrirConexion();
+
+                // Cargar fechas distintas (solo la parte fecha)
+                OleDbCommand cmdFechas = new OleDbCommand(
+                    "SELECT DISTINCT DateValue([Fecha_Hora]) AS Fecha FROM Auditoria_Usuario ORDER BY DateValue([Fecha_Hora])",
+                    bd.ObtenerConexion());
+
+                OleDbDataReader lector = cmdFechas.ExecuteReader();
+
+                cmbDesde.Items.Clear();
+                cmbHasta.Items.Clear();
+
+                var listaFechas = new System.Collections.Generic.List<DateTime>();
+
+                while (lector.Read())
+                {
+                    if (lector["Fecha"] != DBNull.Value)
+                    {
+                        DateTime d = Convert.ToDateTime(lector["Fecha"]);
+                        listaFechas.Add(d);
+                    }
+                }
+
+                foreach (var f in listaFechas)
+                {
+                    string texto = f.ToString("dd/MM/yyyy");
+                    cmbDesde.Items.Add(texto);
+                    cmbHasta.Items.Add(texto);
+                }
+
+                if (cmbDesde.Items.Count > 0)
+                    cmbDesde.SelectedIndex = 0;
+                if (cmbHasta.Items.Count > 0)
+                    cmbHasta.SelectedIndex = cmbHasta.Items.Count - 1;
+
+                lector.Close();
+
+                // Cargar estados fijos (Ingreso / Salida)
+                cmbEstado.Items.Clear();
+                cmbEstado.Items.Add("Todos");
+                cmbEstado.Items.Add("Ingreso");
+                cmbEstado.Items.Add("Salida");
+                cmbEstado.SelectedIndex = 0;
+
+                bd.CerrarConexion();
+            }
+            catch (Exception ex)
+            {
+                try { bd.CerrarConexion(); } catch { }
+                // No interrumpir flujo
+            }
         }
 
         private void CargarListaTablas()
@@ -102,17 +167,53 @@ namespace pryDeganiERP
                 else if (chkbuttonDescendente.Checked)
                     orden = " ORDER BY 1 DESC";
 
-                string consulta = $"SELECT * FROM [{tabla}]" + orden;
-
-                OleDbDataAdapter da = new OleDbDataAdapter(
-                    consulta,
-                    bd.ObtenerConexion());
-
                 DataTable dt = new DataTable();
 
-                da.Fill(dt);
+                // Si estamos viendo la tabla Auditoria_Usuario aplicamos filtros por fecha y estado
+                if (tabla.Equals("Auditoria_Usuario", StringComparison.OrdinalIgnoreCase))
+                {
+                    string where = "";
 
-                dgvAuditoria.DataSource = dt;
+                    var parametros = new System.Collections.Generic.List<OleDbParameter>();
+
+                    if (cmbDesde.SelectedIndex >= 0 && cmbHasta.SelectedIndex >= 0)
+                    {
+                        // Parseamos las fechas en el formato dd/MM/yyyy que usamos en los combos
+                        DateTime desde = DateTime.ParseExact(cmbDesde.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                        DateTime hasta = DateTime.ParseExact(cmbHasta.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture).AddDays(1).AddSeconds(-1);
+
+                        where = " WHERE [Fecha_Hora] BETWEEN ? AND ?";
+                        parametros.Add(new OleDbParameter("@p1", OleDbType.Date) { Value = desde });
+                        parametros.Add(new OleDbParameter("@p2", OleDbType.Date) { Value = hasta });
+                    }
+
+                    if (cmbEstado.SelectedIndex > 0 && !string.IsNullOrEmpty(cmbEstado.Text) && cmbEstado.Text != "Todos")
+                    {
+                        where += string.IsNullOrEmpty(where) ? " WHERE " : " AND ";
+                        where += "[Estado_Login] = ?";
+                        parametros.Add(new OleDbParameter("@p3", OleDbType.VarChar) { Value = cmbEstado.Text });
+                    }
+
+                    string consulta = $"SELECT * FROM [{tabla}]" + where + orden;
+
+                    OleDbDataAdapter da = new OleDbDataAdapter(consulta, bd.ObtenerConexion());
+                    if (parametros.Count > 0)
+                        da.SelectCommand.Parameters.AddRange(parametros.ToArray());
+
+                    da.Fill(dt);
+                    dgvAuditoria.DataSource = dt;
+                }
+                else
+                {
+                    string consulta = $"SELECT * FROM [{tabla}]" + orden;
+
+                    OleDbDataAdapter da = new OleDbDataAdapter(
+                        consulta,
+                        bd.ObtenerConexion());
+
+                    da.Fill(dt);
+                    dgvAuditoria.DataSource = dt;
+                }
 
                 bd.CerrarConexion();
             }
